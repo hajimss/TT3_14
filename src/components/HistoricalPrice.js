ae9012d23ebac6a318908807e487e4c32d9b254f
Ting Zhi Hao <zhihao_ting@outlook.com>
2021-03-05 15:20:19 +0800

    update styling

--- src/components/HistoricalPrice.js
+++ src/components/HistoricalPrice.js
@@ -94,30 +94,34 @@ const HistoricalPrice = () => {
 
   return (
     <div
+      className="container"
       style={{
         width: "1200px",
-        height: "300px",
+        height: "500px",
+        alignItems: "center",
       }}
     >
       <h2>TTK Price History</h2>
-      <LineChart
-        width={1000}
-        height={500}
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="timestamp" />
-        <YAxis dataKey="price" />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="price" stroke="#8884d8" />
-      </LineChart>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={1000}
+          height={500}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="timestamp" />
+          <YAxis dataKey="price" />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="price" stroke="#8884d8" />
+        </LineChart>
+      </ResponsiveContainer>
       {
         <table>
           <tr key={"header"}>
